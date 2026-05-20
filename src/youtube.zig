e8b3ddec721c0adce51d8cd7aee5c737052e7fde
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2026-05-20 11:57:19 +0200

    Refactor fetch and parsing of json response into a separate function

--- src/youtube.zig
+++ src/youtube.zig
@@ -1,3 +1,5 @@
+const base = "https://youtube.googleapis.com/youtube/v3";
+
 pub const Video = struct {
     pub const Statistics = struct {
         views: u64,
@@ -10,33 +12,21 @@ pub fn fetchVideoStatistics(
     client: *std.http.Client,
     gpa: std.mem.Allocator,
     key: []const u8,
-    id: []const u8,
+    video_id: []const u8,
 ) !Video.Statistics {
     var arena_allocator = std.heap.ArenaAllocator.init(gpa);
     const arena = arena_allocator.allocator();
     defer arena_allocator.deinit();
 
-    const base = "https://youtube.googleapis.com/youtube/v3";
     const endpoint = "videos";
     const part = "statistics";
 
-    const url = try std.fmt.allocPrint(arena, "{s}/{s}?part={s}&id={s}&key={s}", .{ base, endpoint, part, id, key });
+    const url = try std.fmt.allocPrint(arena, "{s}/{s}?part={s}&id={s}&key={s}", .{ base, endpoint, part, video_id, key });
 
     var response_writer = std.Io.Writer.Allocating.init(gpa);
     defer response_writer.deinit();
 
-    const result = try client.fetch(.{
-        .method = .GET,
-        .location = .{ .url = url },
-        .headers = .{
-            .accept_encoding = .{ .override = "application/json" },
-        },
-        .response_writer = &response_writer.writer,
-    });
-    if (result.status != .ok)
-        return error.HttpError;
-
-    const Response = struct {
+    const response = try fetchAndParse(client, &response_writer, arena, url, struct {
         items: []struct {
             statistics: struct {
                 viewCount: u64,
@@ -44,15 +34,7 @@ pub fn fetchVideoStatistics(
                 commentCount: u64,
             },
         },
-    };
-
-    const response_str = response_writer.written();
-    const response = std.json.parseFromSliceLeaky(Response, arena, response_str, .{
-        .ignore_unknown_fields = true,
-    }) catch |err| switch (err) {
-        error.OutOfMemory => return error.OutOfMemory,
-        else => return error.InvalidResponse,
-    };
+    });
     if (response.items.len == 0)
         return error.InvalidResponse;
 
@@ -65,6 +47,32 @@ pub fn fetchVideoStatistics(
 
 test fetchVideoStatistics {
     _ = &fetchVideoStatistics;
+}
+
+fn fetchAndParse(
+    client: *std.http.Client,
+    writer: *std.Io.Writer.Allocating,
+    arena: std.mem.Allocator,
+    url: []const u8,
+    comptime T: type,
+) !T {
+    const result = try client.fetch(.{
+        .method = .GET,
+        .location = .{ .url = url },
+        .headers = .{ .accept_encoding = .{ .override = "application/json" } },
+        .response_writer = &writer.writer,
+    });
+    if (result.status != .ok)
+        return error.HttpError;
+
+    const response_str = writer.written();
+    const response = std.json.parseFromSliceLeaky(T, arena, response_str, .{
+        .ignore_unknown_fields = true,
+    }) catch |err| switch (err) {
+        error.OutOfMemory => return error.OutOfMemory,
+        else => return error.InvalidResponse,
+    };
+    return response;
 }
 
 const std = @import("std");
